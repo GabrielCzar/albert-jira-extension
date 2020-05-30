9c19061dd82a48cfd97dfca52314432cd699275d
Gabriel César <gabrielcesar.a.l@gmail.com>
2020-05-30 20:42:38 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 ALBERT_PY_MODULES="$HOME/.local/share/albert/org.albert.extension.python/modules"
 
-if command -v giti >/dev/null 2>&1; then
+if command -v git >/dev/null 2>&1; then
   git clone https://github.com/GabrielCzar/albert-jira-extension.git "$ALBERT_PY_MODULES/albert-jira-extension"
 elif command -v unzip >/dev/null 2>&1; then
   wget -O albert-jira-extension.zip https://github.com/gabrielczar/albert-jira-extension/archive/master.zip
